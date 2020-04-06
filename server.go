88d95bd7fb3acd2188119b4b74821428f317489b
Andrew Mohebbi <amohebbi@mathworks.com>
2020-04-06 14:33:28 -0400

    added concurrency

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-/*A practice server!*/
+//A practice server!
 package main
 
 import (
@@ -6,16 +6,49 @@ import (
 	"fmt"
 	"practicing/functions"
 	"practicing/pelicans"
+	"sync"
 )
+
+var mutex = &sync.Mutex{}
 
 var optional = flag.String("opt", "Hello!", "Tell what to print")
 
 func main() {
+	//Print the statement
 	flag.Parse()
 	fmt.Println(functions.Flip(*optional))
 
+	//Make the pelicans speak
 	p := pelicans.NewPelican("karen", "Kaaaahh")
 	fmt.Println(p.Speak())
-	//p2 := Stringer(*p)
 	fmt.Println(*p)
+
+	fmt.Println(do())
+}
+
+func do() int {
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	var sum int
+
+	go adder(ch1, &sum)
+	go adder(ch2, &sum)
+
+	done1, done2 := <-ch1, <-ch2
+	fmt.Println(done1, done2)
+	return sum
+}
+
+func adder(ch chan int, sum *int) {
+	mutex.Lock()
+
+	for i := 0; i < 10000; i++ {
+		*sum += 1
+	}
+
+	mutex.Unlock()
+
+	ch <- 0 //Tell do() that you're done
+	return
 }
